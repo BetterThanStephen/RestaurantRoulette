55141962ae02de3fcf08ee2f36acee5b14064d49
Charles Clayton <chaeclay@ut.utm.edu>
2018-03-15 09:14:07 -0500

    Change location from hard code to getting device location.

--- app/src/main/java/com/example/lukas/restaurantroulette/MapsActivity.kt
+++ app/src/main/java/com/example/lukas/restaurantroulette/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.example.lukas.restaurantroulette
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,6 +22,8 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    private var locationManager : LocationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,11 +44,61 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+
+        var location : Location? = null
+
+        if(ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED){
+            try{
+                location = locationManager?.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            }
+            catch (e: Exception){
+
+                Log.d("Exception", e.toString())
+            }
+        }
+        else if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            try{
+                location = locationManager?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            }
+            catch (e: Exception){
+
+                Log.d("Exception", e.toString())
+            }
+        }
+        else{
+                Log.d("Location Denied", "Location not found")
+
+        }
+
+
+
+
+
+        var lat = location?.latitude
+        var long = location?.longitude
+
+        if(lat == 0.0 || lat == null){
+            lat = 30.0
+        }
+
+        if(long == 0.0 || long == null){
+            long = -30.0
+        }
+
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(36.3434, 88.8503)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val mapLocation = LatLng(
+                lat,
+                long
+        )
+        mMap.addMarker(MarkerOptions().position(mapLocation).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mapLocation))
+
+        
+
     }
+
 }
